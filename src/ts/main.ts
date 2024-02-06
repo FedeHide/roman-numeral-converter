85a084e4b949b858bfee96c6dcbf1c407e7c5a3b
larcdian <larcdian@gmail.com>
2024-02-06 19:44:25 -0300

    feat: add roman number converter algorithm in TS

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,3 +1,32 @@
-const x: string[] = ['hola', 'mundi', `goasfao`];
+function toRomanNumber(input: number): string {
+	const romanSymbols: Array<[number, string]> = [
+		[1000, 'M'],
+		[900, 'CM'],
+		[500, 'D'],
+		[400, 'CD'],
+		[100, 'C'],
+		[90, 'XC'],
+		[50, 'L'],
+		[40, 'XL'],
+		[10, 'X'],
+		[9, 'IX'],
+		[5, 'V'],
+		[4, 'IV'],
+		[1, 'I'],
+	];
 
-console.log(x);
+	let result: string = '';
+
+	for (const pair of romanSymbols) {
+		const [value, symbol] = pair;
+
+		while (input >= value) {
+			result += symbol;
+			input -= value;
+		}
+	}
+	console.log(result);
+	return result;
+}
+
+toRomanNumber(300);
